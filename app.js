877f9170ac784b49a284425f5cac9e0a39767dee
MoingXTwice <sckbs1314@gmail.com>
2022-04-08 17:21:11 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+
+app.use(cors());
+
+const removeHeader = (req, res, next) => { //x-Powerd-By 제거
+    res.removeHeader("X-Powered-By");
+    next();
+};
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(removeHeader);
+
+const Router = require('./routes');
+app.use("/api", Router);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(3000, () => {
+    console.log(3000, '번으로 서버가 연결되었습니다.');
+});
