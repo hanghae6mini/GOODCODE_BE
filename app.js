74cc0118d1d9b7e8452969aad09aaae0f932266f
rlasuddn <rlasuddn>
2022-04-08 19:46:34 +0900

    test

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const connect = require('./schemas');
-const cookieParser = require('cookie-parser');
-const cors = require('cors')
+const connect = require("./schemas");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+test;
 
 app.use(cors());
 
 connect();
 
-const removeHeader = (req, res, next) => { //x-Powerd-By 제거
-    res.removeHeader("X-Powered-By");
-    next();
+const removeHeader = (req, res, next) => {
+  //x-Powerd-By 제거
+  res.removeHeader("X-Powered-By");
+  next();
 };
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(removeHeader);
 
-const Router = require('./routes');
+const Router = require("./routes");
 app.use("/api", Router);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 module.exports = app;
