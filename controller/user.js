3fe6c4e163e358775d6aa9cdb6b69b9b8177f19c
MoingXTwice <sckbs1314@gmail.com>
2022-04-09 12:41:34 +0900

    [add] 회원가입 기능 추가

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,32 @@
 const User = require('../schemas/user');
+const bcrypt = require('bcrypt');
 
+async function signUp(req, res) {
+    // #swagger.tags = ['User']
+    // #swagger.summary = "회원가입"
 
+    const {userId, nickname, password, validpassword} = req.body
+
+    const existUserId = await User.find({userId})
+    if (existUserId.length) {
+        return res.status(400).json({message: '중복된 아이디가 있습니다.'});
+    }
+
+    const existNickname = await User.find({nickname})
+    if (existNickname.length) {
+        return res.status(400).json({message: '중복된 닉네임이 있습니다.'});
+    }
+
+    if (password !== validpassword) {
+        return res.status(400).json({message: '비밀번호가 일치하지 않습니다.'});
+    }
+
+    const hashPw = bcrypt.hashSync(password, +process.env.SECRET_SALT);
+
+    await User.create({userId, nickname, password: hashPw});
+    res.status(201).json({message: '회원가입이 완료되었습니다.'});
+}
 
 module.exports = {
-
+    signUp
 };
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 const userController = require('../controller/user');
 
+router.post('/', userController.signUp);
+
 module.exports = router;
