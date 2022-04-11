2bf9c0b2f10750053aa71ddee702f9de83ead4f0
rlasuddn <rlasuddn>
2022-04-11 23:37:04 +0900

    미들웨어 추가

--- routes/feed.js
+++ routes/feed.js
@@ -1,14 +1,16 @@
-const router = require('express').Router();
-const feedController = require('../controller/feed');
-const selectFeedMiddleware = require('../middlewares/selectFeed-middleware');
-const insertFeedMiddleware = require('../middlewares/insertFeed-middleware');
-const updateFeedMiddleware = require('../middlewares/updateFeed-middleware');
-const deleteFeedMiddleware = require('../middlewares/deleteFeed-middleware');
+const router = require("express").Router();
+const authMiddle = require("../middlewares/auth-middleware");
+const feedController = require("../controller/feed");
+const selectFeedMiddleware = require("../middlewares/selectFeed-middleware");
+const insertFeedMiddleware = require("../middlewares/insertFeed-middleware");
+const updateFeedMiddleware = require("../middlewares/updateFeed-middleware");
+const deleteFeedMiddleware = require("../middlewares/deleteFeed-middleware");
 
-router.route('/')
-    .get(selectFeedMiddleware, feedController.selectFeed)
-    .post(insertFeedMiddleware, feedController.insertFeed)
-    .put(updateFeedMiddleware, feedController.updateFeed)
-    .delete(deleteFeedMiddleware, feedController.deleteFeed)
+router
+  .route("/")
+  .get(authMiddle, selectFeedMiddleware, feedController.selectFeed)
+  .post(authMiddle, insertFeedMiddleware, feedController.insertFeed)
+  .put(authMiddle, updateFeedMiddleware, feedController.updateFeed)
+  .delete(authMiddle, deleteFeedMiddleware, feedController.deleteFeed);
 
 module.exports = router;
--- routes/feedcomment.js
+++ routes/feedcomment.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const feedCommentController = require("../controller/feedcomment");
+const authMiddle = require("../middlewares/auth-middleware");
 
-router.get("/", feedCommentController.showComments);
-router.post("/", feedCommentController.writeComment);
-router.patch("/", feedCommentController.updateComment);
-router.delete("/", feedCommentController.deleteComment);
+router.get("/", authMiddle, feedCommentController.showComments);
+router.post("/", authMiddle, feedCommentController.writeComment);
+router.patch("/", authMiddle, feedCommentController.updateComment);
+router.delete("/", authMiddle, feedCommentController.deleteComment);
 
 module.exports = router;
