59083dd64ec4942b0822aeca5845b716e9d0bcce
hakseon-yoo <gkrtjs2020@naver.com>
2022-04-11 16:40:47 +0900

    2022. 04. 11. hsyoo. Feed 쪽 개인피드, 전체피드 조회개발된 부분 commit.

---
+++ middlewares/selectFeed-middleware.js
@@ -0,0 +1,24 @@
+const Joi = require('Joi');
+const Sanitizehtml = require('sanitize-html');
+
+module.exports = async (req, res, next) => {
+
+    const validcheck = Joi.object({
+        feedType: Joi.string().required().error(new Error('Server Error - 요청 필수 값인 feedType가 존재하지 않습니다.\n관리자에게 문의하세요.')),
+        userId: Joi.when('feedType', {
+            is: 'user',
+            then: Joi.string().required().error(new Error('Server Error - 요청 필수 값인 userId가 존재하지 않습니다.\n관리자에게 문의하세요.'))
+        }),
+    });
+
+    try{
+        await validcheck.validateAsync(req.query);
+        next();
+    }catch(error){
+        return res.status(400).json({
+            result: 'FAIL',
+            errorMessage: error.message
+        });   
+    } 
+
+}
--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
 const feedController = require('../controller/feed');
+const selectFeedMiddleware = require('../middlewares/selectFeed-middleware');
+
+router.get('/', selectFeedMiddleware, feedController.selectFeed);
 
 module.exports = router;
