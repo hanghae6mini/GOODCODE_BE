ba59807beacd6f637db1954a381e23febd6c3b2e
rlasuddn <rlasuddn>
2022-04-11 23:34:06 +0900

    충돌 수정

--- middlewares/selectFeed-middleware.js
+++ middlewares/selectFeed-middleware.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 const Joi = require("Joi");
-const Sanitizehtml = require("sanitize-html");
-=======
-const Joi = require('Joi');
->>>>>>> master
 
 module.exports = async (req, res, next) => {
   const validcheck = Joi.object({
@@ -14,7 +9,6 @@ module.exports = async (req, res, next) => {
     }),
   });
 
-<<<<<<< HEAD
   try {
     await validcheck.validateAsync(req.query);
     next();
@@ -22,14 +16,6 @@ module.exports = async (req, res, next) => {
     return res.status(400).json({
       result: "FAIL",
       errorMessage: error.message,
-=======
-    const validcheck = Joi.object({
-        feedType: Joi.string().required().error(new Error('Server Error - 요청 필수 값인 feedType가 존재하지 않습니다.\n관리자에게 문의하세요.')),
-        userId: Joi.when('feedType', {
-            is: 'user',
-            then: Joi.string().required().error(new Error('Server Error - 요청 필수 값인 userId가 존재하지 않습니다.\n관리자에게 문의하세요.'))
-        })
->>>>>>> master
     });
   }
 };
