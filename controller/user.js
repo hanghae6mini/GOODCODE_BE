4ecf2ff0615c5d59f0faf1984bdff49c873ec706
MoingXTwice <sckbs1314@gmail.com>
2022-04-14 16:46:47 +0900

    [add] user signup, login valid 추가

--- controller/user.js
+++ controller/user.js
@@ -1,81 +1,104 @@
 const User = require("../schemas/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+
+const signUpValidate = Joi.object({
+    userId: Joi.string().alphanum().min(4).max(16).required().error(new Error('아이디는 4~16자의 영문 대소문자와 숫자를 입력해주세요.')),
+    nickname: Joi.string().pattern(/^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9]+$/).min(2).max(10).required().error(new Error('닉네임은 2~10자의 문자와 숫자를 입력해주세요.')),
+    password: Joi.string().required().min(8).max(16).error(new Error('비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자를 입력해주세요.')),
+    validPassword: Joi.string().valid(Joi.ref('password')).required().error(new Error('비밀번호가 일치하지 않습니다.')),
+});
 
 async function signUp(req, res) {
-  // #swagger.tags = ['User']
-  // #swagger.summary = "회원가입"
+    // #swagger.tags = ['User']
+    // #swagger.summary = "회원가입"
 
-  const { userId, nickname, password, validPassword } = req.body;
+    try {
+        const {userId, nickname, password, validPassword} = await signUpValidate.validateAsync(req.body);
 
-  const existUserId = await User.find({ userId });
-  if (existUserId.length) {
-    return res.status(400).json({ message: "중복된 아이디가 있습니다." });
-  }
+        const existUserId = await User.find({userId});
+        if (existUserId.length) {
+            return res.status(400).json({message: "중복된 아이디가 있습니다."});
+        }
 
-  const existNickname = await User.find({ nickname });
-  if (existNickname.length) {
-    return res.status(400).json({ message: "중복된 닉네임이 있습니다." });
-  }
+        const existNickname = await User.find({nickname});
+        if (existNickname.length) {
+            return res.status(400).json({message: "중복된 닉네임이 있습니다."});
+        }
 
-  if (password !== validPassword) {
-    return res.status(400).json({ message: "비밀번호가 일치하지 않습니다." });
-  }
+        const hashPw = bcrypt.hashSync(password, +process.env.SECRET_SALT);
 
-  const hashPw = bcrypt.hashSync(password, +process.env.SECRET_SALT);
-
-  await User.create({ userId, nickname, password: hashPw });
-  res.status(201).json({ message: "회원가입이 완료되었습니다." });
+        await User.create({userId, nickname, password: hashPw});
+        res.status(201).json({message: "회원가입이 완료되었습니다."});
+    } catch (e) {
+        return res.status(400).json({
+            result: 'FAIL',
+            message: e.message,
+        })
+    }
 }
 
+const loginValidate = Joi.object({
+    userId: Joi.string().required().error(new Error('아이디를 입력해주세요.')),
+    password: Joi.string().error(new Error('비밀번호를 입력해주세요.')),
+});
+
 async function login(req, res) {
-  // #swagger.tags = ['User']
-  // #swagger.summary = "로그인"
+    // #swagger.tags = ['User']
+    // #swagger.summary = "로그인"
 
-  const { userId, password } = req.body;
+    try {
+        const {userId, password} = await loginValidate.validateAsync(req.body);
 
-  // 아이디 비교
-  const user = await User.findOne({ userId });
-  if (!user) {
-    return res.status(401).json({ result: "fail", errorMessage: "아이디 또는 비밀번호가 일치하지 않습니다." });
-  }
+        // 아이디 비교
+        const user = await User.findOne({userId});
+        if (!user) {
+            return res.status(401).json({result: "fail", errorMessage: "아이디 또는 비밀번호가 일치하지 않습니다."});
+        }
 
-  // 비밀번호 비교
-  const hashPw = bcrypt.compareSync(password, user.password); // 일치하면 true 틀리면 false
-  if (!hashPw) {
-    return res.status(401).json({ result: "fail", errorMessage: "아이디 또는 비밀번호가 일치하지 않습니다." });
-  }
+        // 비밀번호 비교
+        const hashPw = bcrypt.compareSync(password, user.password); // 일치하면 true 틀리면 false
+        if (!hashPw) {
+            return res.status(401).json({result: "fail", errorMessage: "아이디 또는 비밀번호가 일치하지 않습니다."});
+        }
 
-  // jwtToken 생성
-  const token = jwt.sign(
-    {
-      userId: user.userId,
-      nickname: user.nickname,
-    },
-    process.env.SECRET_KEY,
-    {
-      expiresIn: "60m",
-      issuer: "GOODCODE",
+        // jwtToken 생성
+        const token = jwt.sign(
+            {
+                userId: user.userId,
+                nickname: user.nickname,
+            },
+            process.env.SECRET_KEY,
+            {
+                expiresIn: "60m",
+                issuer: "GOODCODE",
+            }
+        );
+
+        res.status(200).send({token, message: '로그인이 완료되었습니다.'});
+    } catch (e) {
+        return res.status(400).json({
+            result: 'FAIL',
+            message: e.message,
+        })
     }
-  );
-
-  res.status(200).send({token, message: '로그인이 완료되었습니다.'});
 }
 
 async function authUser(req, res) {
-  try {
-    const { user } = res.locals;
-    res.status(200).json({
-      userId: user.userId,
-      nickname: user.nickname,
-    });
-  } catch (err) {
-    res.status(400).json({ result: "FAIL", message: "유저를 확인할 수 없습니다." });
-  }
+    try {
+        const {user} = res.locals;
+        res.status(200).json({
+            userId: user.userId,
+            nickname: user.nickname,
+        });
+    } catch (err) {
+        res.status(400).json({result: "FAIL", message: "유저를 확인할 수 없습니다."});
+    }
 
 }
 
-async function modifyProfile(req, res){
+async function modifyProfile(req, res) {
     // #swagger.tags = ['User']
     // #swagger.summary = "유저 프로필 수정"
 
@@ -104,7 +127,7 @@ async function getUser(req, res) {
 
     const {userId} = res.locals.user;
 
-    const user  = await User.findOne({userId});
+    const user = await User.findOne({userId});
 
     res.status(200).json({
         user: {
@@ -115,21 +138,14 @@ async function getUser(req, res) {
             url: user.url,
         },
         result: 'SUCCESS',
-        message: '정보 수정이 완료되었습니다.'});
+        message: '정보 수정이 완료되었습니다.'
+    });
 }
 
-/*
-nickname:
-introduce:
-location:
-email:
-url:
- */
-
 module.exports = {
-  signUp,
-  login,
-  authUser,
-  modifyProfile,
-  getUser
+    signUp,
+    login,
+    authUser,
+    modifyProfile,
+    getUser
 };
