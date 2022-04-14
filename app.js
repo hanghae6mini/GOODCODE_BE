526b8018507b30d1f123d25941e0a48269aad0c8
MoingXTwice <sckbs1314@gmail.com>
2022-04-14 11:55:40 +0900

    [fix]cors 설정 변경

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(morgan("combined"));
 connect();
 
